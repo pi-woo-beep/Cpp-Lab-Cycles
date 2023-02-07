834dbf63e955789722ec0f4e54790df821e72d04
Pi Woo Beep <120108404+pi-woo-beep@users.noreply.github.com>
2023-02-07 16:05:15 +0530

    Update string- compare, concatenate and length.cpp
    extra..

--- string- compare, concatenate and length.cpp
+++ string- compare, concatenate and length.cpp
@@ -65,3 +65,57 @@ int main(){
 	}
 	return(0);
 }
+
+
+/*
+#include <iostream>
+using namespace std;
+
+class STRING{
+    private:
+        string s,s1,s2;
+    public:
+        void comp();
+        void concatenate();
+        void len();
+};
+
+void STRING::comp(){
+    cout<<"Enter string 1: ";
+    cin>>s1;
+    cout<<"Enter string 2: ";
+    cin>>s2;
+    int x=s1.compare(s2);
+    if (x==0){
+        cout<<"Both strings are the same :)";
+    }
+    else{
+        cout<<"Both strings are not the same :(";
+    }
+}
+
+void STRING::concatenate(){
+    cout<<"Enter string 1: ";
+    cin>>s1;
+    cout<<"Enter string 2: ";
+    cin>>s2;
+    cout<<"The resultant string is: "<<s1+s2;
+}
+
+void STRING::len(){
+    cout<<"Enter string: ";
+    cin>>s;
+    cout<<"Length of string: "<<s.length();
+}
+
+int main(){
+    int ch;
+    
+    while (ch!=5){
+        cout<<"\n1. Compare 2 strings. \n2. Concatenate 2 strings. \n3. Check the length of a given string. \n4. Exit."<<endl;
+        switch (ch){
+            case 1:{
+        }
+    }
+}
+*/
