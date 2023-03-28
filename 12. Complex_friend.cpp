51014d19695b6cff2fe48418f980af5443af388e
Pi Woo Beep <120108404+pi-woo-beep@users.noreply.github.com>
2023-03-28 14:32:15 +0530

    Update and rename complex_friend.cpp to 12. Complex_friend.cpp

--- 12. Complex_friend.cpp
+++ 12. Complex_friend.cpp
@@ -6,40 +6,42 @@ complex representing the sum of two complex numbers.*/
 #include <iostream>
 using namespace std;
 
-class complex{
+class comp{
     private:
         int real, imag;
     public:
-        complex (int a, int b){
+        comp (int a, int b){
             real=a;
             imag=b;
         }
-        friend complex sum(complex, complex);
+        friend comp sum(comp, comp);
         void display(){
             cout<<"Sum= "<<real<<"+"<<imag<<"i"<<endl;
         }
 };
 
-complex sum(complex a, complex b){
-    complex c;
-    c.real=a.real+b.real;
-    c.imag=a.imag+b.imag;
-    return c;
+comp sum(comp a, comp b){
+    int x,y;
+    comp C(x,y);
+    C.real=a.real+b.real;
+    C.imag=a.imag+b.imag;
+    return C;
 }
 
 int main(){
     int real,imag;
     cout<<"Enter 2 integers for the first complex number: ";
     cin>>real>>imag;
-    complex A(real,imag);
+    comp A(real,imag);
 
     cout<<"Enter 2 integers for the second complex number: ";
     cin>>real>>imag;
-    complex B(real,imag);
-
-    complex c;
-    c=sum(A,B);
-    c.display();
+    comp B(real,imag);
+    
+    int a,b;
+    comp C(a,b);
+    C=sum(A,B);
+    C.display();
 
     return 0;
 }
